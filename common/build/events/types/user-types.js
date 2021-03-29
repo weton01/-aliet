7cc45461b10c0966aa8a42b0a35bad028dd38ff9
Wellington Campana Ferreira <wellington_ferreira280@hotmail.com>
2021-03-29 15:30:11 -0300

    Updates

--- common/build/events/types/user-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserTypes = void 0;
-var UserTypes;
-(function (UserTypes) {
-    UserTypes["Admin"] = "admin";
-    UserTypes["Operator"] = "operator";
-    UserTypes["User"] = "user";
-})(UserTypes = exports.UserTypes || (exports.UserTypes = {}));
